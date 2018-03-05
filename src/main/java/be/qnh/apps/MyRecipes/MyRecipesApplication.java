035437eec36b598b49b93817d561f58a77d97294
JaValerie <valerie.meers@gmail.com>
2018-03-05 17:21:06 +0100

    Edit Recipe, Timing, Component

--- src/main/java/be/qnh/apps/MyRecipes/MyRecipesApplication.java
+++ src/main/java/be/qnh/apps/MyRecipes/MyRecipesApplication.java
@@ -1,12 +1,38 @@
 package be.qnh.apps.MyRecipes;
 
+import be.qnh.apps.MyRecipes.domain.Component;
+import be.qnh.apps.MyRecipes.domain.Ingredient;
+import be.qnh.apps.MyRecipes.domain.Recipe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class MyRecipesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRecipesApplication.class, args);
-	}
+
+        Recipe recept = new Recipe();
+        recept.setCategory("Cupcake");
+        recept.setDescription("Tasty cupcake with pumpkin");
+        recept.setCourse("Sweets");
+        recept.setEvaluation(5);
+        recept.setKitchen("vegan");
+        recept.setName("Pumpkin cupcakes");
+        recept.setServingSize(16);
+        recept.setLevel("Beginner");
+        recept.setSource("Chloe Coscarelli");
+
+        Component componentA = new Component();
+        List<Ingredient> ingredients = new ArrayList<>();
+        componentA.setIngredients(ingredients);
+
+
+    }
+
+
+
 }
--- src/main/java/be/qnh/apps/MyRecipes/domain/Recipe.java
+++ src/main/java/be/qnh/apps/MyRecipes/domain/Recipe.java
@@ -1,16 +1,19 @@
 package be.qnh.apps.MyRecipes.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Entity
-public class Recipe {
+public class Recipe implements Serializable {
+    private static final long serialVersionID = 56498795462246L;
+
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
 
-
+    @Basic(optional = false)
     private String name;
     private String description;
     private String course;
@@ -19,8 +22,119 @@ public class Recipe {
     private String kitchen;
     private String source;
     private int servingSize;
-    private Timing time;
+    @Embedded
+    private Timing time = new Timing();
     private int evaluation;
-    private Component component;
 
+    @Basic(optional = false)
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private Collection<Component> component;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getKitchen() {
+        return kitchen;
+    }
+
+    public void setKitchen(String kitchen) {
+        this.kitchen = kitchen;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public int getServingSize() {
+        return servingSize;
+    }
+
+    public void setServingSize(int servingSize) {
+        this.servingSize = servingSize;
+    }
+
+    public Timing getTime() {
+        return time;
+    }
+
+    public void setTime(Timing time) {
+        this.time = time;
+    }
+
+    public int getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(int evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    public Collection<Component> getComponent() {
+        return component;
+    }
+
+    public void setComponent(ArrayList<Component> component) {
+        this.component = component;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", course='" + course + '\'' +
+                ", evaluation=" + evaluation +
+                '}';
+    }
 }
