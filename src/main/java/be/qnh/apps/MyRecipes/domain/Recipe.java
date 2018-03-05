a2204cc53cd12c054857843a352f853e625dd620
JaValerie <valerie.meers@gmail.com>
2018-03-05 16:44:47 +0100

    Revert "Edit Recipe, Timing, Component"
    This reverts commit b1af51b

--- src/main/java/be/qnh/apps/MyRecipes/domain/Recipe.java
+++ src/main/java/be/qnh/apps/MyRecipes/domain/Recipe.java
@@ -1,19 +1,16 @@
 package be.qnh.apps.MyRecipes.domain;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-public class Recipe implements Serializable {
-    private static final long serialVersionID = 56498795462246L;
-
+public class Recipe {
     @Id
     @GeneratedValue
-    private Long id;
+    private long id;
 
-    @Basic(optional = false)
+
     private String name;
     private String description;
     private String course;
@@ -22,119 +19,8 @@ public class Recipe implements Serializable {
     private String kitchen;
     private String source;
     private int servingSize;
-    @Embedded
-    private Timing time = new Timing();
+    private Timing time;
     private int evaluation;
+    private Component component;
 
-    @Basic(optional = false)
-    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
-    private Collection<Component> component;
-
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getKitchen() {
-        return kitchen;
-    }
-
-    public void setKitchen(String kitchen) {
-        this.kitchen = kitchen;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public int getServingSize() {
-        return servingSize;
-    }
-
-    public void setServingSize(int servingSize) {
-        this.servingSize = servingSize;
-    }
-
-    public Timing getTime() {
-        return time;
-    }
-
-    public void setTime(Timing time) {
-        this.time = time;
-    }
-
-    public int getEvaluation() {
-        return evaluation;
-    }
-
-    public void setEvaluation(int evaluation) {
-        this.evaluation = evaluation;
-    }
-
-    public Collection<Component> getComponent() {
-        return component;
-    }
-
-    public void setComponent(ArrayList<Component> component) {
-        this.component = component;
-    }
-
-    @Override
-    public String toString() {
-        return "Recipe{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", course='" + course + '\'' +
-                ", evaluation=" + evaluation +
-                '}';
-    }
 }
--- src/main/java/be/qnh/apps/MyRecipes/domain/Timing.java
+++ src/main/java/be/qnh/apps/MyRecipes/domain/Timing.java
@@ -1,33 +1,4 @@
 package be.qnh.apps.MyRecipes.domain;
 
-import javax.persistence.Embeddable;
-
-@Embeddable
-public class Timing  {
-    private int prepTime;
-    private int cookTime;
-
-    public int getPrepTime() {
-        return prepTime;
-    }
-
-    public void setPrepTime(int prepTime) {
-        this.prepTime = prepTime;
-    }
-
-    public int getCookTime() {
-        return cookTime;
-    }
-
-    public void setCookTime(int cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Timing{" +
-                "prepTime=" + prepTime +
-                ", cookTime=" + cookTime +
-                '}';
-    }
+public class Timing {
 }
