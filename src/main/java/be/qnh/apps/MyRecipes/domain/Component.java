b1af51b0aa26be589600bbc2280fc81d8dbf1bc0
JaValerie <valerie.meers@gmail.com>
2018-03-05 16:44:07 +0100

    Edit Recipe, Timing, Component

--- src/main/java/be/qnh/apps/MyRecipes/domain/Component.java
+++ src/main/java/be/qnh/apps/MyRecipes/domain/Component.java
@@ -1,4 +1,56 @@
 package be.qnh.apps.MyRecipes.domain;
 
-public class Component {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+public class Component implements Serializable {
+    private static final long serialVersionID = 56498795462258L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    @ElementCollection
+    @OneToMany(mappedBy = "component", cascade = CascadeType.ALL)
+    private Collection<Ingredient> ingredients = new ArrayList<>();
+    private String instructions;
+
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Collection<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Collection<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    @Override
+    public String toString() {
+        return "Component{" +
+                "id=" + id +
+                ", ingredients=" + ingredients +
+                ", instructions='" + instructions + '\'' +
+                '}';
+    }
 }
--- src/main/java/be/qnh/apps/MyRecipes/domain/Timing.java
+++ src/main/java/be/qnh/apps/MyRecipes/domain/Timing.java
@@ -1,4 +1,33 @@
 package be.qnh.apps.MyRecipes.domain;
 
-public class Timing {
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Timing  {
+    private int prepTime;
+    private int cookTime;
+
+    public int getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(int prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(int cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Timing{" +
+                "prepTime=" + prepTime +
+                ", cookTime=" + cookTime +
+                '}';
+    }
 }
